dc75c6d10fe850d50af5665432a2d183384b8cf8
Patrick Schleizer <adrelanos@riseup.net>
2019-12-28 02:49:18 -0500

    debugging

--- script.sh
+++ script.sh
@@ -28,7 +28,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ## Functions ##################################################################
-
+set -x
 set -eu
 
 Info () {
